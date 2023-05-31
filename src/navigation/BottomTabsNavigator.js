7bf8dfda7a73805d2939000c2f5b7473902561e2
Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
2023-05-31 15:22:35 -0300

    mangas (#5)

--- src/navigation/BottomTabsNavigator.js
+++ src/navigation/BottomTabsNavigator.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import Manga from "../pages/Manga"
 const Tab = createBottomTabNavigator()
 
 function BottomTabsNavigator() {
@@ -8,6 +9,7 @@ function BottomTabsNavigator() {
         <Tab.Navigator>
             <Tab.Screen name='Home' component={Home} options={{ headerShown: false }} />
             <Tab.Screen name='Register' component={Register} options={{ headerShown: false }} />
+            <Tab.Screen name='Mangas' component={Manga} options={{ headerShown: false }} />
         </Tab.Navigator>
     )
 }
